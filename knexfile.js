1493e2b439294072e0bb44d071b9beeba15ba132
bahargumuser <121288876+bahargumuser@users.noreply.github.com>
2023-05-18 17:08:27 +0300

    typo düzeltildi

--- knexfile.js
+++ knexfile.js
@@ -4,22 +4,44 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
+
   development: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: "./dev.sqlite3",
-    },
-    migrations:{
-      directory: "./data/migrations"
-    },
-    seeds:{
-      directory: "./data/seeds"
-    }
-    useNullAsDefault:true,
-    pool:{
-      afterCreate:(conn, done) =>  {
-        conn.run("PRAGMA foreign_keys = ON", done)
-      }
+      filename: './dev.sqlite3'
     }
   },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
 };
