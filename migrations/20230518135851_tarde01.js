789f367e07ebe45ba5cf6f919ca9da13f64892eb
bahargumuser <121288876+bahargumuser@users.noreply.github.com>
2023-05-18 17:18:18 +0300

    migration dosyası oluşturuldu

--- migrations/20230518135851_tarde01.js
+++ migrations/20230518135851_tarde01.js
@@ -2,15 +2,10 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-    const all
-  
-};
+exports.up = function (knex) {};
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
