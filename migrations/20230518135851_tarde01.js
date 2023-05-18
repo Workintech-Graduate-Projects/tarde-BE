66da808e9d1ad8f9cb5eefcdc4173d27ba91ef0c
bahargumuser <121288876+bahargumuser@users.noreply.github.com>
2023-05-18 23:55:11 +0300

    4 tablo oluşturdu, 2 tablo kaldı

--- migrations/20230518135851_tarde01.js
+++ migrations/20230518135851_tarde01.js
@@ -2,7 +2,27 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  const all = knex.schema
+    .createTable("Sehir", (tablo) => {
+      tablo.increments("sehir_id"); //1-1 arttırmasını belirttiğimiz ifade sadece pk kolonlarında belirtiyoruz.
+      tablo.string("sehir_adi").notNullable(); //acaba string mi olmalıydı --evet string olmalıydı
+    })
+    .createTable("Merkez", (tablo) => {
+      tablo.increments("merkez_id");
+      tablo.string("merkez_adi").notNullable();
+      tablo.integer("sehir_id").references("sehir_id").inTable("Sehir");
+      tablo.string("merkez_adres");
+    })
+    .createTable("Personel", (tablo) => {
+      tablo.increments("personel_id").unique(), tablo.string("personel_adi");
+    })
+    .createTable("MerkezTelefon", (tablo) => {
+      tablo.increments("merkez_telefon_id");
+      tablo.integer("merkez_id").references("merkez_id").inTable("Merkez");
+      tablo.string("telefon").notNullable();
+    });
+};
 
 /**
  * @param { import("knex").Knex } knex
