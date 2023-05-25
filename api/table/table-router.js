e6a4196126f0f56aa442344a76d23d01c6e5158a
Ömür <omurcanuslu@outlook.com>
2023-05-25 18:13:35 +0300

    merkez id eklendi

--- api/table/table-router.js
+++ api/table/table-router.js
@@ -1,2 +1,29 @@
+const router = require("express").Router();
+const model =require("./table-model");
+
+
+router.get(
+    "/:id",
+    async (req, res, next) => {
+      try {
+     const merkezler = await model.getById(req.params.id);
+     const personel = await model.getByPersonel(req.params.id);
+     res.status(200).json(merkezler.concat(personel))
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+router.get(
+    "/coordinate",
+    async (req, res, next) => {
+      try {
+       return getCoordinates
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+
 
   module.exports = router;
